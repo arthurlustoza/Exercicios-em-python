5842a2997dad85e74891d4d71cc1a5dffdaa74a8
unknown <arthurlustosa47@gmail.com>
2026-03-02 09:41:14 -0400

    mais  alguns exercicios

---
+++ Resolução_de_exercícios/Exercícios_github_1_estrutura_decisão/Exercício_4.py
@@ -0,0 +1,12 @@
+"""
+Faça um Programa que verifique se uma letra digitada é vogal ou consoante.
+"""
+
+print("Verificando se e vogal ou consoante")
+
+letra = str(input("Digite a letra desejada: "))
+
+if letra == "a" and "b" and "c" and "d":
+    print("Vogal")
+else:
+    print("Consoante")
