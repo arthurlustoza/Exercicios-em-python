245e5fbd5adf6bdf2cbc3747e7c542480459d6a9
unknown <arthurlustosa47@gmail.com>
2026-03-06 11:06:53 -0400

    Exs

---
+++ Resolução_de_exercícios/Exercícios_github_1_estrutura_decisão/Exercício_18.py
@@ -0,0 +1,26 @@
+"""
+Exercício 17 da seção de estrutura de decisão da Python Brasil:
+https://wiki.python.org.br/EstruturaDeDecisao
+
+Faça um Programa que peça um número correspondente a um determinado ano e em seguida informe se este ano é ou não
+bissexto.
+    >>> eh_ano_bissexto(400)
+    True
+    >>> eh_ano_bissexto(800)
+    True
+    >>> eh_ano_bissexto(2100)
+    False
+    >>> eh_ano_bissexto(2004)
+    True
+    >>> eh_ano_bissexto(2022)
+    False
+
+"""
+
+print("Verifica se o ano de Bissexto!")
+
+ano = int(input("Digite o ano: "))
+if (ano % 4 == 0 and ano % 100 != 0) or (ano % 400 == 0):
+    print("Ano bissexto")
+else:
+    print("Ano não bissexto")
